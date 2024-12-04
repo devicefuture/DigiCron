c8ce2a4748480ddd59149a7f9049a916c58a0806
James-Livesey <jamesliveg@gmail.com>
2024-12-04 18:38:53 +0000

    Generate WASM imports for app lib

--- tools/api/api.sh
+++ tools/api/api.sh
@@ -2,10 +2,12 @@
 
 namespace timing
     class EarthTime
+        constructor "int" year "unsigned int" month "unsigned int" day "unsigned int" hour "unsigned int" minute "unsigned int" second
+        INTERNAL_NAME=dc_timing_EarthTime_newUsingMilliseconds constructor "int" year "unsigned int" month "unsigned int" day "unsigned long" millisecondOfDay
+
         method "unsigned int" daysInYear
         method "unsigned int" daysInMonth
         method "unsigned long" millisecondsInDay
-        method "struct LeapAdjustment" leapAdjustmentToday
 
         method "bool" inLeapMillisecond
         method "unsigned long" postLeapMillisecondOffset
@@ -38,22 +40,26 @@ namespace ui
     enum PenMode OFF ON
 
     class Icon
-        method "void" setPixel "unsigned int" x "unsigned int" y "PenMode" value
+        constructor
+
+        method "void" setPixel "unsigned int" x "unsigned int" y "ENUM PenMode" value
 
     class Screen
+        constructor
+
         method "void" clear
         method "void" setPosition "unsigned int" column "unsigned int" row
-        method "void" setPixel "unsigned int" x "unsigned int" y "PenMode" value
-        method "void" print "char" c
+        method "void" setPixel "unsigned int" x "unsigned int" y "ENUM PenMode" value
+        INTERNAL_NAME=dc_ui_Screen_printName method "void" print "char" c
         method "void" print "char*" chars
-        method "void" print "String" string
-        method "void" print "Icon" icon
-        method "void" printRepeated "String" string "unsigned int" times
-        method "void" scroll "String" string "unsigned int" maxLength
+        INTERNAL_NAME=dc_ui_Screen_printString method "void" print "STRING" string
+        INTERNAL_NAME=dc_ui_Screen_printIcon method "void" print "CLASS Icon" icon
+        method "void" printRepeated "STRING" string "unsigned int" times
+        method "void" scroll "STRING" string "unsigned int" maxLength
         method "void" resetScroll
-        method "void" rect "unsigned int" x1 "unsigned int" y1 "unsigned int" x2 "unsigned int" y2 "PenMode" value
-        method "void" filledRect "unsigned int" x1 "unsigned int" y1 "unsigned int" x2 "unsigned int" y2 "PenMode" value
+        method "void" rect "unsigned int" x1 "unsigned int" y1 "unsigned int" x2 "unsigned int" y2 "ENUM PenMode" value
+        method "void" filledRect "unsigned int" x1 "unsigned int" y1 "unsigned int" x2 "unsigned int" y2 "ENUM PenMode" value
 
         method "void" open "bool" urgent
         method "void" close
-        method "void" swapWith "Screen*" currentScreen
+        method "void" swapWith "CLASSPTR Screen" currentScreen
